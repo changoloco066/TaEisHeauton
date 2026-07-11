1683930e0bf48089f3fa3046565f0d00a7e55038
changoloco066 <bigoteschicos@gmail.com>
2026-07-11 13:42:28 -0600

    feat: estructura inicial del proyecto (incompleto)

---
+++ app/src/main/java/com/example/taeisheauton/parser/MeditationParser.java
@@ -0,0 +1,17 @@
+package com.example.taeisheauton.parser;
+
+import com.example.taeisheauton.model.meditation;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MeditationParser {
+    private ParserState state;
+    private int currentBook;
+    private int currentNumber;
+    private StringBuilder currentText;
+    private List<Meditation> meditations;
+
+    public List<Meditation> parse (String input){
+
+    }
+}
